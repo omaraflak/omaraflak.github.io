4f970bbc4268f7906c095e34ddf025d6f768c660
Omar Aflak <aflakomar@gmail.com>
2025-07-22 23:49:50 +0100

    clean up renderer

--- src/renderer.py
+++ src/renderer.py
@@ -7,6 +7,11 @@ import datetime
 METADATA_PATTERN = r"^:(.+):(.+)$"
 
 
+def read_template() -> str:
+    with open("article.html", "r") as file:
+        return file.read()
+
+
 def strip(text: str, sub: str) -> str:
     while True:
         tmp = text.removeprefix(sub).removesuffix(sub)
@@ -19,15 +24,15 @@ def make_tag(title: str) -> str:
     return title.lower().replace(" ", "-")
 
 
+def format_date(year: int, month: int, day: int) -> str:
+    date = datetime.datetime(year, month, day)
+    return date.strftime("%b %d, %Y")
+
+
 def parse_image_size(location: str) -> tuple[str, str, str]:
     parts = location.split(";")
     mapping = dict(part.split("=") for part in parts[1:])
     return parts[0], mapping.get("h", ""), mapping.get("w", "")
-
-
-def format_date(year: int, month: int, day: int) -> str:
-    date = datetime.datetime(year, month, day)
-    return date.strftime("%b %d, %Y")
 
 
 def parse_metadata(text: str, metadata: dict[str, str]) -> bool:
@@ -39,18 +44,18 @@ def parse_metadata(text: str, metadata: dict[str, str]) -> bool:
     return False
 
 
-def parse_elements(elements: list[marko.block.Element], metadata: dict[str, str]) -> str:
-    return "".join(parse_element(element, metadata) for element in elements)
+def render_elements(elements: list[marko.block.Element], metadata: dict[str, str]) -> str:
+    return "".join(render_element(element, metadata) for element in elements)
 
 
-def parse_element(element: marko.block.Element, metadata: dict[str, str]) -> str:
+def render_element(element: marko.block.Element, metadata: dict[str, str]) -> str:
     if isinstance(element, marko.inline.RawText):
         if not parse_metadata(element.children, metadata):
             return element.children
     elif isinstance(element, marko.block.Paragraph):
-        return parse_elements(element.children, metadata)
+        return render_elements(element.children, metadata)
     elif isinstance(element, marko.block.Heading):
-        title = parse_elements(element.children, metadata)
+        title = render_elements(element.children, metadata)
         if element.level == 1:
             return f'<h2 id={make_tag(title)} class="article-section">{title}</h2>'
         elif element.level == 2:
@@ -58,27 +63,27 @@ def parse_element(element: marko.block.Element, metadata: dict[str, str]) -> str
         else:
             print(f"Heading with level {element.level} is not supported!")
     elif isinstance(element, marko.block.Quote):
-        quote = parse_elements(element.children, metadata)
+        quote = render_elements(element.children, metadata)
         return f'<div class="article-quote">{quote}</div>'
     elif isinstance(element, marko.block.List):
-        tmp = parse_elements(element.children, metadata)
+        tmp = render_elements(element.children, metadata)
         return f'<ul>{tmp}</ul>'
     elif isinstance(element, marko.block.ListItem):
-        tmp = parse_elements(element.children, metadata)
+        tmp = render_elements(element.children, metadata)
         return f'<li class="article-li">{tmp}</li>'
     elif isinstance(element, marko.block.ThematicBreak):
         return '<div class="article-hr"></div>'
     elif isinstance(element, marko.inline.Emphasis):
-        text = parse_elements(element.children, metadata)
+        text = render_elements(element.children, metadata)
         return f'<span class="article-italic">{text}</span>'
     elif isinstance(element, marko.inline.StrongEmphasis):
-        text = parse_elements(element.children, metadata)
+        text = render_elements(element.children, metadata)
         return f'<span class="article-bold">{text}</span>'
     elif isinstance(element, marko.inline.Link):
-        title = parse_elements(element.children, metadata)
+        title = render_elements(element.children, metadata)
         return f'<a class="article-link" href="{element.dest}">{title}</a>'
     elif isinstance(element, marko.inline.Image):
-        alt = parse_elements(element.children, metadata)
+        alt = render_elements(element.children, metadata)
         url, height, width = parse_image_size(element.dest)
         return f'<center><img class="article-image" height="{height}" width="{width}" src="{url}" alt="{alt}"></center>'
     elif isinstance(element, marko.block.BlankLine):
@@ -88,7 +93,7 @@ def parse_element(element: marko.block.Element, metadata: dict[str, str]) -> str
     elif isinstance(element, marko.inline.CodeSpan):
         return f'<code class="article-code-inline">{element.children}</code>'
     elif isinstance(element, marko.block.FencedCode):
-        code = parse_elements(element.children, metadata)
+        code = render_elements(element.children, metadata)
         return f'<pre class="article-code-block {element.lang}"><code>{code}</code></pre>'
     elif isinstance(element, marko.block.HTMLBlock):
         return element.body
@@ -97,9 +102,9 @@ def parse_element(element: marko.block.Element, metadata: dict[str, str]) -> str
     return ""
 
 
-def parse_document(document: marko.block.Document) -> tuple[str, dict[str, str]]:
+def render_document(document: marko.block.Document) -> tuple[str, dict[str, str]]:
     metadata = dict()
-    html = parse_elements(document.children, metadata)
+    html = render_elements(document.children, metadata)
     html = strip(html, "<p></p>")
     html = strip(html, "</br>")
     html = strip(html, "<p></p>")
@@ -107,12 +112,8 @@ def parse_document(document: marko.block.Document) -> tuple[str, dict[str, str]]
     return html, metadata
 
 
-def read_template() -> str:
-    return open("article.html", "r").read()
-
-
 def to_html(markdown: str) -> str:
-    content, metadata = parse_document(marko.parse(markdown))
+    content, metadata = render_document(marko.parse(markdown))
     date = format_date(
         int(metadata["year"]),
         int(metadata["month"]),
