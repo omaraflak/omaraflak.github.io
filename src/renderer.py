7f00cd8ceb812461c2eea17c5ceb9d0e040ce8b8
Omar Aflak <aflakomar@gmail.com>
2025-07-22 23:46:25 +0100

    Put article template in file

--- src/renderer.py
+++ src/renderer.py
@@ -4,51 +4,6 @@ import marko.inline
 import datetime
 
 
-TEMPLATE = """
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{title}}</title>
-    <link rel="stylesheet" href="../styles/index.css">
-    <link rel="stylesheet" href="../styles/fonts.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.7.0/styles/github.min.css">
-</head>
-
-<body>
-    <div id="header"></div>
-    <div class="article-header">
-        <div class="article-header-content">
-            <h1 class="article-title">{{title}}</h1>
-            <p class="article-description">{{description}}</p>
-            <p class="article-date">{{date}}</p>
-        </div>
-    </div>
-    <main class="article-main">
-        <article class="article-body">
-            {{content}}
-        </article>
-    </main>
-    <div class="article-hr"></div>
-    <div id="footer"></div>
-    <script src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.js" onload="renderMathInElement(document.body, {
-        delimiters: [
-            {left: '$$', right: '$$', display: true},
-            {left: '$', right: '$', display: false},
-        ]
-    });"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js"></script>
-    <script>hljs.highlightAll();</script>
-    <script src="../scripts/common.js"></script>
-</body>
-
-</html>
-"""
-
 METADATA_PATTERN = r"^:(.+):(.+)$"
 
 
@@ -152,6 +107,10 @@ def parse_document(document: marko.block.Document) -> tuple[str, dict[str, str]]
     return html, metadata
 
 
+def read_template() -> str:
+    return open("article.html", "r").read()
+
+
 def to_html(markdown: str) -> str:
     content, metadata = parse_document(marko.parse(markdown))
     date = format_date(
@@ -159,7 +118,8 @@ def to_html(markdown: str) -> str:
         int(metadata["month"]),
         int(metadata["day"])
     )
-    html = TEMPLATE.replace("{{title}}", metadata["title"])
+    html = read_template()
+    html = html.replace("{{title}}", metadata["title"])
     html = html.replace("{{description}}", metadata["description"])
     html = html.replace("{{date}}", date)
     html = html.replace("{{content}}", content)
