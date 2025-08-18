ff4933f435943ff7b887e1dca11b4a1a35a4d300
Omar Aflak <aflakomar@gmail.com>
2025-08-18 08:36:18 +0100

    update sitemap

--- src/templates.py
+++ src/templates.py
@@ -106,11 +106,11 @@ HIGHLIGHT_STYLES = """
 SITEMAP = """
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://omaraflak.github.io/</loc>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
-    </url>
+<url>
+    <loc>https://omaraflak.github.io/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+</url>
 {{entries}}
 </urlset>
 """
